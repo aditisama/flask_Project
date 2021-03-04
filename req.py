d6b88ec2f34fb3a3e221ab6548c87f1de69d41b5
aditisama <57286739+aditisama@users.noreply.github.com>
2021-03-04 11:37:55 +0530

    Initial commit

---
+++ req.py
@@ -0,0 +1,12 @@
+from flask import *
+app = Flask(__name__)
+@app.route('/')
+def customer():
+    return render_template('customer.html')
+@app.route('/success', methods=['POST', 'GET'])
+def print_data():
+    if request.method == 'POST':
+        result = request.form
+        return render_template("data.html", result=result)
+if __name__ == '__main__':
+    app.run(debug=True)
